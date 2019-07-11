ca6d7a62eba373c3c5b18ff9da04d9191cac4b38
Dan226 <laragomezdaniel@gmail.com>
2019-07-11 17:10:29 +0000

    S6C3

--- S6C3/LaraDaniel_cuerdayTambor.cpp
+++ S6C3/LaraDaniel_cuerdayTambor.cpp
@@ -12,9 +12,9 @@ double tambor(int len, double h);
 int main(){
     double h = 0.01;   
     double min_x = 0;
-    double max_x = 1;
+    double max_x = 1.0;
     int i;
-    int L = 1;
+    double L = 1.0;
     int len = ((max_x-min_x)/h);
     pde(len,h);
     pdelibre(len,h);
@@ -241,70 +241,77 @@ double pdeoscilacion(int len, double h, int L){
     double u_present[len];
     double delta_a, delta_t, c, r;
     double t1[len],t2[len],t3[len],t4[len],t5[len], t6[len];
+    a[0] = 0.0;
     a[1] = h;
     int i;
     float A0 = 0.01;
     for(i = 2; i <= len; i++){
         a[i]  = a[i-1] + h;
     }  
-    for(i = 1; i <= len; i++){
+    for(i = 0; i < len; i++){
         u_inicial[i]  = 0;
     }      
     delta_a = 0.005;  
     c = 300.0;
-    delta_t = (delta_a/c)*0.25;
+    delta_t = (h/c)*0.25;
     int iter = 0.1/delta_t;
-    r = 0.00125;
+    r = c*delta_t/h;
+    
+    cout << iter << endl;
     
     u_inicial[0] = 0.0;
-    u_inicial[len] = 0.0;
+    u_inicial[len-1] = 0.0;
     
-    for(i = 1; i <= len; i++){
+    
+    
+    for(i = 0; i <= len-1; i++){
         u_future[i] = 0.0;
      }
-    for(i = 2; i <= (len-1); i++){
+    for(i = 1; i < (len-1); i++){
         u_future[i] = u_inicial[i]+(r*r/2.0)*(u_inicial[i+1]-2.0*u_inicial[i]+u_inicial[i-1]);
     }      
-    for(i = 1; i <= len; i++){
+    for(i = 0; i < len; i++){
         u_past[i] = u_inicial[i];
         u_present[i] = u_future[i];
      }
-    for(int j = 1; j <= iter; j++){
+    for(int j = 0; j <= iter; j++){
         
-        for(i = 2; i <= (len-1); i++){
-            u_future[i] = (2.0* (1 - r*r))*u_present[i] - u_past[i] + (r*r)*(u_present[i+1] +  u_present[i-1]);     
+        for(i = 1; i < (len-1); i++){
+            u_future[i] = r*r*(u_present[i+1] +  u_present[i-1]- 2*u_present[i]) - u_past[i] + 2*u_present[i];     
         }    
-        u_future[len] = A0*cos((3.0*c*j*3.1415167)/L);
-        for(i = 0; i <= len; i++){    
+    
+        u_future[len-1] = A0*sin((3.0*c*j*delta_t*3.1415167)/L);
+        for(i = 0; i < len; i++){    
             u_past[i] = u_present[i];
             u_present[i] = u_future[i];
+        
         }
        if(j == 1000){
            for(i = 1; i <= len; i++){
             t6[i] = u_future[i];
           }
        }
-        else if(j == 5000){
+        else if(j == 2000){
             for(i = 1; i <= len; i++){
             t1[i] = u_future[i];
             }
         } 
-        else if(j == 8000){
+        else if(j == 3000){
             for(i = 1; i <= len; i++){
             t2[i] = u_future[i];
             }
         }
-        else if(j == 15000){
+        else if(j == 4000){
             for(i = 1; i <= len; i++){
             t3[i] = u_future[i];
             }
         }
-        else if(j == 20000){
+        else if(j == 5000){
             for(i = 1; i <= len; i++){
             t4[i] = u_future[i];
             }
         }
-        else if(j == 24000){
+        else if(j == 6000){
             for(i = 1; i <= len; i++){
             t5[i] = u_future[i];
             }
