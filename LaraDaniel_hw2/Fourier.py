bb7a929ea24e9ca8ed05e76f60dc669ad75d1431
Dan226 <laragomezdaniel@gmail.com>
2019-07-12 17:32:20 +0000

    Tercero

--- LaraDaniel_hw2/Fourier.py
+++ LaraDaniel_hw2/Fourier.py
@@ -10,72 +10,78 @@ imagen2 = plt.imread("cara_03_grisesMF.png")
 
 ########Transformada de Fourier usando el paquete de scipy
 df21 = fft2(imagen1)
-f21 = np.fft.fftshift(imagen1)
+f21 = np.fft.fftshift(df21)
 df22 = fft2(imagen2)
-f22 = np.fft.fftshift(imagen2)
+f22 = np.fft.fftshift(df22)
 
 ######## Se grafica el espectro de Fourier
 plt.figure()
-a = plt.imshow(abs(df21), norm = LogNorm())
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+a = plt.imshow(abs(f21), norm = LogNorm())
 cbar = plt.colorbar(a)
 plt.xlabel("w")
 plt.ylabel("F(w)")
 plt.title("Espectro de Fourier imagen 1")
-plt.savefig("FFT-2D-Imagen1")
-
-plt.figure()
-b = plt.imshow(abs(df22), norm = LogNorm())
-cbar = plt.colorbar(a)
+plt.subplot(1,2,2)
+b = plt.imshow(abs(f22), norm = LogNorm())
+cbar = plt.colorbar(b)
 plt.xlabel("w")
 plt.ylabel("F(w)")
 plt.title("Espectro de Fourier imagen 2")
-plt.savefig("FFT-2D-Imagen2")
+plt.savefig("FFT-2D-Imagenes")
 
 ######## Se realizan filtros pasa-altos y pasa-bajos para realizar la imagen híbrida.
 def filtro1(fc, amp):
-    for i in range(np.shape(amp)[1]):
-        for j in range(np.shape(amp)[0]):
-            if(amp[j,i]>fc):
-                amp[j,i] = 0
+    for i in range(np.shape(amp)[0]):
+        for j in range(np.shape(amp)[1]):
+            if(amp[i,j]>fc):
+                amp[i,j] = 0
     return amp
 
 def filtro2(fc, amp):
-    for i in range(np.shape(amp)[1]):
-        for j in range(np.shape(amp)[0]):
-            if(amp[j,i]<fc):
-                amp[j,i] = 0
+    for i in range(np.shape(amp)[0]):
+        for j in range(np.shape(amp)[1]):
+            if(amp[i,j]<fc):
+                amp[i,j] = 0
     return amp
 
-fc = 100
-freqfiltro1 = filtro1(fc,df21)  
-freqfiltro2 = filtro2(100,df22)
+freqfiltro1 = filtro1(55,df21)  
+freqfiltro2 = filtro2(4,df22)
+
 
 plt.figure()
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
 a = plt.imshow(abs(freqfiltro1), norm = LogNorm(), vmin = 100, vmax = 1200)
 cbar = plt.colorbar(a)
 plt.xlabel("w")
 plt.ylabel("F(w)")
-plt.title("Filtro imagen 1")
-plt.savefig("FFT2Dfiltro1")   
-
-plt.figure()
+plt.title("Filtro imagen 1")  
+plt.subplot(1,2,2)
 b = plt.imshow(abs(freqfiltro2), norm = LogNorm(), vmin = 100, vmax = 1200)
 cbar = plt.colorbar(b)
 plt.xlabel("w")
 plt.ylabel("F(w)")
 plt.title("Filtro imagen 2")
-plt.savefig("FFT2Dfiltro2")   
+plt.savefig("FFT2Dfiltro")   
  
 ###########Transformada inversa
 filtrototal = np.zeros([np.shape(df21)[0],np.shape(df21)[1]])
 
-for i in range(np.shape(df21)[1]):
-    for j in range(np.shape(df21)[0]):
-        filtrototal[j][i] = abs(freqfiltro1[j][i] + freqfiltro2[j][i])
+i1 = np.fft.ifftshift(freqfiltro1)
+i2 = np.fft.ifftshift(freqfiltro2)
+
+filtrototal = i1+i2
+
+#for i in range(np.shape(df21)[1]):
+#    for j in range(np.shape(df21)[0]):
+#        filtrototal[j][i] = np.fft.ifftshift(freqfiltro1[j][i]) + np.fft.ifftshift(freqfiltro2[j][i])
 
 if2 = ifft2(filtrototal)
 
 plt.figure()
-plt.imshow(abs(if2), cmap = "gist_gray", interpolation = "bessel")
+plt.figure(figsize=(20,10))
+plt.imshow(abs(if2),cmap = "gray",  norm = LogNorm())
 plt.savefig("ImFiltrada")
 
